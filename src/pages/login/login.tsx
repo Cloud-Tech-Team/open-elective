633354a39ff260fa7264cb36a6745f7f0180bd7b
Glitchyi <advaithnarayanan8@gmail.com>
2024-06-28 00:21:20 +0530

    spelling mistakes

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,12 +1,12 @@
-import Instructions from "./Instructions.tsx";
-import Forms from "@/pages/login/Form.tsx";
+import Instructions from "./Instructions";
+import Form from "./Form";
 
 const Login = () => {
   return (
     <>
       <div className=" w-[100%] min-h-screen md:h-screen  md:py-[1em bg-slate-900  backdrop:blur-[15px] relative flex gap-[1em] justify-center items-center text-white flex-col md:flex-row">
         <Instructions />
-        <Forms />
+        <Form />
       </div>
     </>
   );
