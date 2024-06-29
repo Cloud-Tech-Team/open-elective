25f09a47bf51d110e88d5590193ae3dfbd1d3779
Glitchyi <advaithnarayanan8@gmail.com>
2024-06-30 02:47:34 +0530

    re added login

---
+++ src/pages/Login/Form.tsx
@@ -0,0 +1,91 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { User } from "@/types/User";
+
+function Form() {
+  const navigate = useNavigate();
+  const [user, setUser] = useState<User>({
+    name: "",
+    email: "",
+    registerId: "",
+    department: "",
+    optedCourses: null,
+  });
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setUser((prevUser) => ({  
+      ...prevUser,
+      [name]: value,
+    }));
+  };
+
+  return (
+    <form
+      className="flex flex-col gap-2 w-[80%]"
+      onSubmit={(e) => {
+        e.preventDefault();
+        const emailRegex = /^\d\d\w\w\d\d\d@mgits\.ac\.in$/;
+        if (!emailRegex.test(user.email)) {
+          alert("Please enter a valid college email id");
+          return;
+        }
+        user.department = user.email?.slice(2, 4).toUpperCase() ?? "";
+        fetch(`${import.meta.env.VITE_URL}/register`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            name: user.name,
+            email: user.email,
+            registerId: user.registerId,
+            department: user.department,
+            optedCourses: null,
+          }),
+        }).then((response) => {
+          if (response.status === 412){
+            alert("User already registered");
+            return;
+          }
+          localStorage.setItem("userInfo", JSON.stringify(user));
+          setUser({ name: "", email: "", registerId: "",department:"", optedCourses: null});
+          navigate("/home");
+        })
+      }}
+    >
+      <input
+        type="text"
+        name="name" // Add name attribute
+        required
+        placeholder="Student Name ( Advaith Narayanan )"
+        value={user.name}
+        onChange={handleInputChange} // Bind onChange
+        className="cubic-1 drop-shadow input p-2 bg-transparent border-b-2 border-white text-white hover:placeholder:text-dark-offset rounded placeholder:text-white/80"
+      />
+      <input
+        type="email"
+        name="email" // Add name attribute
+        required
+        placeholder="College Email ID ( 21CS051@mgits.ac.in )"
+        value={user.email}
+        onChange={handleInputChange} // Bind onChange
+        className="cubic-1 drop-shadow input p-2 bg-transparent border-b-2 border-white text-white hover:placeholder:text-dark-offset rounded placeholder:text-white/80"
+      />
+      <input
+        type="text"
+        name="registerId" // Add name attribute
+        required
+        placeholder="Register No ( MUT21CS051 )"
+        value={user.registerId}
+        onChange={handleInputChange} // Bind onChange
+        className="cubic-1 drop-shadow input p-2 bg-transparent border-b-2 border-white text-white hover:placeholder:text-dark-offset rounded placeholder:text-white/80"
+      />
+      <button className="p-2 font-bold text-[1.4rem] bg-white rounded text-dark-offset mt-[2em]">
+        Next
+      </button>
+    </form>
+  );
+}
+
+export default Form;
---
+++ src/pages/Login/Instructions.tsx
@@ -0,0 +1,22 @@
+const Instructions = () => {
+    return (
+        <div className='md:w-[50%] w-[100%] h-[80vh] md:h-screen  bg-dark-offset/40  flex round flex-col justify-center items-center'>
+            <h1 className='text-[4rem] font-medium'>Instructions</h1>
+
+            <div className=' relative px-4 md:px-[1em] md:leading-loose'>
+                <div className='bg-[#f9e69a] text-black leading-snug rounded font-medium px-[1.5em] py-[1.5em] mt-[3em] mb-3 '>
+                    <h3> <i className="ri-file-warning-fill"></i> <b>Crucial Information:</b></h3>
+                    <p>Read Before Proceeding</p>
+                    <p>To ensure a smooth and successful process, please take a moment to carefully review these essential instructions:</p>
+                </div>
+                <ul className='flex flex-col justify-between gap-5 py-5'>
+                    <li><b className='important'>Data loss: </b> Following these steps precisely is critical to safeguard your data. Any deviations could lead to permanent data loss.</li>
+                    <li><b className='important'>Final Choices: </b>Once you've made your selections, they become permanent and cannot be edited or changed later. Choose wisely!</li>
+                    <li> <b className='important'>Avoid Refreshing: </b> Refrain from refreshing the page during the selection process. Doing so can cause data loss or errors.</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Instructions
